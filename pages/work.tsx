9384ce38fbfa55dd9212810fc05efa45c7df7392
cerebrium <nicholas.m.shankland@gmail.com>
2020-12-06 14:53:22 -0800

    some chagnes

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,13 +1,51 @@
 import styles from '../styles/Work.module.scss'
+import React, { useEffect, useState} from 'react'
+import { useRouter } from 'next/router'
 
 const Work: React.FunctionComponent = () => {
+    // allow for routing
+    const router = useRouter()
+
+    // state
+    const [ homeClassname, setHomeClassName ] = useState<string>('aboutContainer')
+
+    useEffect( () => {
+        if (styles) {
+            setHomeClassName(styles.aboutContainer)
+        }
+    }, [])
+
+    // function for changing the classname for about
+    const handleHomeClick = () => {
+        setHomeClassName(styles.homeMakeBig)
+        setTimeout( () => {
+            router.push('/')
+        }, 3000)
+    }
     return (
         <div className={styles.workMainContainer}>
+            <div className={styles.circleContainerLeft}>
+                <div 
+                    className={homeClassname}
+                    onClick={handleHomeClick}
+                >
+                    <div className={styles.textContainer}>
+                        <h2>Home</h2>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.circleContainerRight}>
+                <div className={styles.workContainer}>
+                    <div className={styles.textContainer}>
+                        <h2>Work</h2>
+                    </div>
+                </div>
+            </div>
             <h1 className={styles.workTitle}>Work</h1>
-            <div className={styles.workDisplayContainer}>
+            <div className={styles.workContainerTwo}>
                 {/* giftata */}
-                <div>
-                    Giftata
+                <div className={styles.giftataContainer}>
+                    <h3>Giftata</h3>
                     <div>
                         <img 
                             src="/giftataScreenshot.png" 
@@ -33,12 +71,58 @@ const Work: React.FunctionComponent = () => {
                     </div>
                 </div>
                 {/* daves designs */}
-                <div>
-                    Daves Designs
+                <div className={styles.daveContainer}>
+                    <h3>Daves Designs</h3>
+                    <div>
+                        <img 
+                            src="/dave/daveMain.png" 
+                            alt=""
+                            className={styles.workImages}
+                        />
+                    </div>
+                    <div className={styles.lowerImageContainer}>
+                        <div>
+                            <img 
+                            src="/dave/daveProducts.png" 
+                            alt=""
+                            className={styles.lowerLeftImage}
+                            />
+                        </div>
+                        <div>
+                            <img 
+                            src="/dave/daveContact.png" 
+                            alt=""
+                            className={styles.lowerLeftImage}
+                            />
+                        </div>
+                    </div>
                 </div>
                 {/* crm */}
-                <div>
-                    Crm
+                <div className={styles.crmContainer}>
+                    <h3>Crm</h3>
+                    <div>
+                        <img 
+                            src="/crm/crmMain.png" 
+                            alt=""
+                            className={styles.workImages}
+                        />
+                    </div>
+                    <div className={styles.lowerImageContainer}>
+                        <div>
+                            <img 
+                            src="/crm/crmRota.png" 
+                            alt=""
+                            className={styles.lowerLeftImage}
+                            />
+                        </div>
+                        <div>
+                            <img 
+                            src="/crm/crmDailyService.png" 
+                            alt=""
+                            className={styles.lowerLeftImage}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
