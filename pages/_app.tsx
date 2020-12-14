6fd4ec62aca834348e2fdc7952ea20c7520f396c
cerebrium <nicholas.m.shankland@gmail.com>
2020-12-14 15:57:32 -0800

    changed favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.scss'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/octocat.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
